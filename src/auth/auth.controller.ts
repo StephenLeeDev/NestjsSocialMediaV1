553bcceaf21403f6ab2aad9f4b45e4def2e17ea0
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-09-04 15:33:17 +0900

    Feat | Apply JWT Validation On Board Controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,6 +2,8 @@ import { Body, Controller, Post, Req, UseGuards, ValidationPipe } from '@nestjs/
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
+import { GetUser } from './get-user.decorator';
+import { User } from './user.entity';
 
 @Controller('auth')
 export class AuthController {
@@ -19,8 +21,8 @@ export class AuthController {
 
     @Post('/test')
     @UseGuards(AuthGuard())
-    test(@Req() req) {
-        console.log('req', req);
+    test(@GetUser() user: User) {
+        console.log('user', user);
     }
 
 }
