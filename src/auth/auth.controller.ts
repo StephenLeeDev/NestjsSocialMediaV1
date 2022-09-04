7b701d6d64803e9beed7594c1468b376cd70961e
StephenLeeDev <stephen.lee.dev@gmail.com>
2022-09-04 14:35:39 +0900

    Feat | Sign In Feature Implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 
@@ -8,6 +8,12 @@ export class AuthController {
     
     @Post('/signup')
     signUp(@Body() authCredentialsDTO: AuthCredentialsDto): Promise<void> {
-        return this.authService.signUp(authCredentialsDTO)
+        return this.authService.signUp(authCredentialsDTO);
     }
+
+    @Post('/signin')
+    signIn(@Body(ValidationPipe) AuthCredentialsDto: AuthCredentialsDto) {
+        return this.authService.signIn(AuthCredentialsDto);
+    }
+
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,7 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,8 +11,19 @@ export class AuthService {
         private userRepository: UserRepository,
     ) { }
     
-    async signUp(AuthCredentialsDTO: AuthCredentialsDto): Promise<void> {
-        return this.userRepository.createUser(AuthCredentialsDTO);
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return this.userRepository.createUser(authCredentialsDto);
+    }
+
+    async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+        const { username, password } = authCredentialsDto;
+        const user = await this.userRepository.findOne({ username })
+
+        if (user && (await bcrypt.compare(password, user.password))) {
+            return 'LogIn Sucecess'
+        } else {
+            throw new UnauthorizedException('LogIn Failed')
+        }
     }
 
 }
