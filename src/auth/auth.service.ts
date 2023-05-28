94a1bd74d3740040dc9aaa8bb54c71ef1d51fe72
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-05-28 17:56:51 +0900

    Fix : Same email, different platform exception handling implementation

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -16,50 +16,70 @@ export class AuthService {
     private logger = new Logger('AuthService');
 
     async signUp(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
-        const { username, socialType, email } = authCredentialsDto;
+        const { email } = authCredentialsDto;
 
-        const user = await this.userRepository.findOne({ email })
+        const user = await this.userRepository.findOne({ email });
 
+        // User account exists.
         if (user) {
-            throw new ConflictException(`LogIn Failed. ${email} account already exists.`)
+            this.logger.error(`Sign up Failed. ${email} account already exists in ${user.socialType}.`);
+            throw new ConflictException(`Sign up Failed. ${email} account already exists in ${user.socialType}.`)
         }
+        // User account not exists. Sign in directly.
         else {
             await this.userRepository.createUser(authCredentialsDto);
             const accessToken = await this.jwtService.sign({ email });
+
+            this.logger.verbose(`User ${user.email} has signed in`);
+
             return { accessToken };
         }
     }
 
     async signIn(authCredentialsDto: AuthCredentialsDto): Promise<{accessToken: string}> {
-        const { username, socialType, email } = authCredentialsDto;
-        const user = await this.userRepository.findOne({ email })
+        const { socialType, email } = authCredentialsDto;
+        const user = await this.userRepository.findOne({ email });
 
+        // User account exists.
         if (user) {
-            const payload = { email };
-            const accessToken = await this.jwtService.sign(payload);
-
-            this.logger.verbose(`User ${user.email} has signed in`);
-
-            return { accessToken };
+            // User account exists.
+            if (user.socialType == socialType) {
+                const payload = { email };
+                const accessToken = await this.jwtService.sign(payload);
+    
+                this.logger.verbose(`User ${user.email} has signed in`);
+    
+                return { accessToken };
+            }
+            else {
+                this.logger.error(`Sign in Failed. ${email} account already exists in ${user.socialType}.`);
+                throw new ConflictException(`Sign in Failed. ${email} account already exists in ${user.socialType}.`);
+            }
         } else {
-            throw new UnauthorizedException('LogIn Failed')
+            this.logger.error('Sign in Failed');
+            throw new UnauthorizedException('Sign in Failed');
         }
     }
 
     async test(): Promise<{ accessToken: string }> {
         const username = 'testUser';
         const email = 'testUser@gmail.com';
-        const user = await this.userRepository.findOne({ email })
+        const user = await this.userRepository.findOne({ email });
 
         if (user) {
             const accessToken = await this.jwtService.sign({ email });
+
+            this.logger.verbose(`User ${user.email} has signed in`);
+    
             return { accessToken };
         } else {
-            this.logger.verbose(`The textUser not exists`);
             const testUserAuthDto = { username, socialType: AuthSocialType.GOOGLE, email };
             await this.signUp(testUserAuthDto);
 
             const accessToken = await this.jwtService.sign({ email });
+
+            this.logger.verbose(`User ${user.email} has signed in`);
+    
             return { accessToken };
         }
     }
