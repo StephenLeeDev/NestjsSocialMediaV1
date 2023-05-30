63baa142825488d78b569fde127af74403de3580
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-05-30 10:44:09 +0900

    Hotfix: Resolve merge conflicts

--- src/configs/typeorm.config.ts
+++ src/configs/typeorm.config.ts
@@ -6,7 +6,7 @@ export const typeORMConfig: TypeOrmModuleOptions = {
     port: 4000,
     username: 'postgres',
     password: '0000',
-    database: 'social-media-app',
+    database: 'social-media-app-v1',
     entities: [__dirname + '/../**/*.entity.{js,ts}'],
     synchronize: true
 }
