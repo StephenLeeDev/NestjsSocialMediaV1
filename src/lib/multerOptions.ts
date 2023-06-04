6b6904a637e582ccbc61191cf7fad3855e7f30c4
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-06-04 22:27:05 +0900

    Fix : Fix image file path error

--- src/lib/multerOptions.ts
+++ src/lib/multerOptions.ts
@@ -1,14 +1,12 @@
-// 업로드 가능한 확장자 설정
 export const imageFileFilter = (req, file, callback) => {
-    if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
-      return callback(new Error("Only image files are allowed!"), false);
-    }
-    callback(null, true);
-  };
-  
-  // 파일명 중복을 피하기 위해 파일명 수정
-  export const editFileName = (req, file, callback) => {
-    const fileExtName = file.originalname.slice(((file.originalname.lastIndexOf(".") - 1) + 2));
-    const time = Date.now();
-    callback(null, `${time}.${fileExtName}`);
-  };
+  if (!file.originalname.match(/\.(jpg|jpeg|png)$/)) {
+    return callback(new Error("Only image files are allowed!"), false);
+  }
+  callback(null, true);
+};
+
+export const editFileName = (req, file, callback) => {
+  const fileExtName = file.originalname.slice(((file.originalname.lastIndexOf(".") - 1) + 2));
+  const time = Date.now();
+  callback(null, `${time}.${fileExtName}`);
+};
