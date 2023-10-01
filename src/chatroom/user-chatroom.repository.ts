f206e923093aa06397d14c0fd8a07baa5447cac1
StephenLeeDev <stephen.lee.dev@gmail.com>
2023-10-01 14:05:31 +0900

    Feature : Fetch chat room list feature implementation
    - Implement [getChatRoomList] to fetch the chat room list

--- src/chatroom/user-chatroom.repository.ts
+++ src/chatroom/user-chatroom.repository.ts
@@ -2,6 +2,8 @@ import { EntityRepository, Repository } from "typeorm";
 import { UserChatRoomEntity } from "./user-chatroom.entity";
 import { User } from "src/user/user.entity";
 import { ChatRoomEntity } from "./chatroom.entity";
+import { ChatRoomDto, ChatRoomListDto } from "./dto/chatroom.dto";
+import { MessageEntity } from "src/message/message.entity";
 
 @EntityRepository(UserChatRoomEntity)
 export class UserChatRoomRepository extends Repository<UserChatRoomEntity> {
@@ -29,6 +31,54 @@ export class UserChatRoomRepository extends Repository<UserChatRoomEntity> {
         }
     
         return null;
+    }
+
+    async getChatRoomList(user: User, page: number, limit: number): Promise<ChatRoomListDto> {
+        const query = this.createQueryBuilder('userChatRoomEntity')
+            .leftJoinAndSelect('userChatRoomEntity.user', 'user')
+            .leftJoinAndSelect('userChatRoomEntity.chatRoom', 'chatRoom')
+            .where('user.email = :userEmail', { userEmail: user.email })
+            .groupBy('chatRoom.id, message.id')
+            .groupBy('chatRoom.id')
+            .addGroupBy('userChatRoomEntity.id')
+            .addGroupBy('user.email')
+            .orderBy('chatRoom.updatedAt', 'DESC')
+            .skip((page - 1) * limit)
+            .take(limit);
+
+        const [list, total] = await query.getManyAndCount();
+
+        const chatRooms: ChatRoomEntity[] = list.map(userChatRoom => userChatRoom.chatRoom);
+
+        const chatRoomList: ChatRoomDto[] = chatRooms.map((item: ChatRoomEntity) => {
+            const chatRoomInfo: ChatRoomDto = new ChatRoomDto();
+            chatRoomInfo.id = item.id;
+            chatRoomInfo.participants;
+            chatRoomInfo.createdAt = item.createdAt;
+            chatRoomInfo.updatedAt = item.updatedAt;
+            return chatRoomInfo;
+        });
+      
+        return { list: chatRoomList, total };
+    }
+    
+    async findChatPartnerByChatRoomId(chatRoomId: number, user: User): Promise<User> {
+        const userChatRoom = await this.createQueryBuilder('userChatRoom')
+            .leftJoinAndSelect('userChatRoom.user', 'user')
+            .where('userChatRoom.chatRoomId = :chatRoomId', { chatRoomId })
+            .andWhere('user.email != :userEmail', { userEmail: user.email })
+            .getOne();
+
+        return userChatRoom.user;
+    }
+
+    async findParticipantsByChatRoomId(chatRoomId: number): Promise<User[]> {
+        const userChatRooms = await this.createQueryBuilder('userChatRoom')
+            .leftJoinAndSelect('userChatRoom.user', 'user')
+            .where('userChatRoom.chatRoomId = :chatRoomId', { chatRoomId })
+            .getMany();
+
+        return userChatRooms.map(userChatRoom => userChatRoom.user);
     }
 
 }
